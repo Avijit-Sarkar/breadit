ecc17e73196785207ce1da39aa35efd82d3b3f49
Avijit-Sarkar <103516653+Avijit-Sarkar@users.noreply.github.com>
2023-06-23 00:13:51 +0530

    authentication working

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,4 @@
-import { NextAuthOptions } from "next-auth";
+import { NextAuthOptions, getServerSession } from "next-auth";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { db } from "./db";
 import GoogleProvider from "next-auth/providers/google";
@@ -65,3 +65,5 @@ export const authOptions: NextAuthOptions = {
     },
   },
 };
+
+export const getAuthSession = () => getServerSession(authOptions);
