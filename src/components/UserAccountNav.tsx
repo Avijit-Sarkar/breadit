11b347d31cbacc252ec3e9f3e920cd703ed52635
Avijit-Sarkar <103516653+Avijit-Sarkar@users.noreply.github.com>
2023-06-23 11:58:07 +0530

    creating userAvatar

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -1,12 +1,23 @@
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+} from "@radix-ui/react-dropdown-menu";
 import { User } from "next-auth";
 import { FC } from "react";
+import UserAvatar from "./UserAvatar";
 
 interface UserAccountNavProps {
   user: Pick<User, "name" | "image" | "email">;
 }
 
 const UserAccountNav: FC<UserAccountNavProps> = ({ user }) => {
-  return <div>UserAccountNav</div>;
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <UserAvatar user={user} />
+      </DropdownMenuTrigger>
+    </DropdownMenu>
+  );
 };
 
 export default UserAccountNav;
