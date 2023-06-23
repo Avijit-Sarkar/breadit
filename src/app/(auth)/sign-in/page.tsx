ea52935eadd1e72bf2f141af80e01102e02353e8
Avijit-Sarkar <103516653+Avijit-Sarkar@users.noreply.github.com>
2023-06-23 20:43:36 +0530

    protecting the sign in page

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,11 +1,18 @@
 import SignIn from "@/components/SignIn";
 import { buttonVariants } from "@/components/ui/Button";
+import { getAuthSession } from "@/lib/auth";
 import { cn } from "@/lib/utils";
 import { ChevronLeft } from "lucide-react";
 import Link from "next/link";
-import { FC } from "react";
+import { redirect } from "next/navigation";
 
-const page: FC = () => {
+const page = async () => {
+  const session = await getAuthSession();
+
+  if (session) {
+    redirect("/");
+  }
+
   return (
     <div className="absolute inset-0">
       <div className="h-full max-w-2xl mx-auto flex flex-col items-center justify-center gap-20">
