d7244c69a81f6490efb1cc471147159fbb8cc4da
Avijit-Sarkar <103516653+Avijit-Sarkar@users.noreply.github.com>
2023-06-25 16:07:39 +0530

    handling errors and on success

--- src/app/r/[slug]/layout.tsx
+++ src/app/r/[slug]/layout.tsx
@@ -89,7 +89,10 @@ const Layout = async ({
               ) : null}
 
               {subreddit.creatorId !== session?.user.id ? (
-                <SubscribeLeaveToggle />
+                <SubscribeLeaveToggle
+                  subredditId={subreddit.id}
+                  subredditName={subreddit.name}
+                />
               ) : null}
             </dl>
           </div>
