7b7e450ab145720e4cfc9c84e184a1232c9d75e4
Avijit-Sarkar <103516653+Avijit-Sarkar@users.noreply.github.com>
2023-06-27 00:55:24 +0530

    initialised the editor

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { FC, useCallback, useRef } from "react";
+import { FC, useCallback, useEffect, useRef, useState } from "react";
 import TextareaAutosize from "react-textarea-autosize";
 import { PostCreationRequest, PostValidator } from "@/lib/validators/post";
 import { useForm } from "react-hook-form";
@@ -26,6 +26,14 @@ const Editor: FC<EditorProps> = ({ subredditId }) => {
   });
 
   const ref = useRef<EditorJS>();
+  const [isMounted, setIsMounted] = useState<boolean>(false);
+  const _titleRef = useRef<HTMLTextAreaElement>(null);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setIsMounted(true);
+    }
+  }, []);
 
   const initializeEditor = useCallback(async () => {
     const EditorJS = (await import("@editorjs/editorjs")).default;
@@ -81,14 +89,42 @@ const Editor: FC<EditorProps> = ({ subredditId }) => {
       });
     }
   }, []);
+
+  useEffect(() => {
+    const init = async () => {
+      await initializeEditor();
+
+      setTimeout(() => {
+        // set focus to title
+      });
+    };
+
+    if (isMounted) {
+      init();
+
+      return () => {};
+    }
+  }, [isMounted, initializeEditor]);
+
+  const { ref: titleRef, ...rest } = register("title");
+
   return (
     <div className="w-full p-4 bg-zinc-50 rounded-lg border border-zinc-200">
       <form id="subreddit-post-form" className="w-fit" onSubmit={() => {}}>
         <div className="prose prose-stone dark:prose-invert">
           <TextareaAutosize
+            ref={(e) => {
+              titleRef(e);
+
+              // @ts-ignore
+              _titleRef.current = e;
+            }}
+            {...rest}
             placeholder="Title"
             className=" w-full resize-none appearance-none overflow-hidden bg-transparent text-5xl font-bold focus:outline-none"
           />
+
+          <div id="editor" className="min-h-[500px]" />
         </div>
       </form>
     </div>
