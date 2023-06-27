8b2bf10661078c00f50f8eb56a01d29296a91728
Avijit-Sarkar <103516653+Avijit-Sarkar@users.noreply.github.com>
2023-06-27 20:53:00 +0530

    displaying posts & creating infinite scrolling

---
+++ src/components/PostFeed.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import { ExtendedPost } from "@/types/db";
+import { FC, useRef } from "react";
+import { useIntersection } from "@mantine/hooks";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { INFINITE_SCROLLING_PAGINATION_RESULT } from "@/config";
+import axios from "axios";
+import { useSession } from "next-auth/react";
+import Post from "./Post";
+
+interface PostFeedProps {
+  initalPosts: ExtendedPost[];
+  subredditName?: string;
+}
+
+const PostFeed: FC<PostFeedProps> = ({ initalPosts, subredditName }) => {
+  const lastPostRef = useRef<HTMLElement>(null);
+  const { ref, entry } = useIntersection({
+    root: lastPostRef.current,
+    threshold: 1,
+  });
+
+  const { data: session } = useSession();
+
+  const { data, fetchNextPage, isFetchingNextPage } = useInfiniteQuery(
+    ["infinite-query"],
+    async ({ pageParam }) => {
+      const query =
+        `/api/posts?limit=${INFINITE_SCROLLING_PAGINATION_RESULT}&page=${pageParam}` +
+        (!!subredditName ? `&subredditName=${subredditName}` : "");
+
+      const { data } = await axios.get(query);
+      return data as ExtendedPost[];
+    },
+    {
+      getNextPageParam: (_, pages) => {
+        return pages.length + 1;
+      },
+      initialData: { pages: [initalPosts], pageParams: [1] },
+    }
+  );
+
+  const posts = data?.pages.flatMap((page) => page) ?? initalPosts;
+
+  return (
+    <ul className="flex flex-col col-span-2 space-y-6">
+      {posts.map((post, index) => {
+        const votesAmt = post.votes.reduce((acc, vote) => {
+          if (vote.type === "UP") return acc + 1;
+          if (vote.type === "DOWN") return acc - 1;
+          return acc;
+        }, 0);
+
+        const currentVote = post.votes.find(
+          (vote) => vote.userId === session?.user.id
+        );
+
+        if (index === posts.length - 1) {
+          return (
+            <li key={post.id} ref={ref}>
+              <Post />
+            </li>
+          );
+        } else {
+          // eslint-disable-next-line react/jsx-key
+          return <Post />;
+        }
+      })}
+    </ul>
+  );
+};
+
+export default PostFeed;
